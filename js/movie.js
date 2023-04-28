2f5ab98c44b2342ddc174cf39c3c21ddbfd5d35e
TarazonaTec <brayan.tarazona.sandoval@gmail.com>
2023-04-27 21:56:51 -0500

    ultimos lanzamientos, busqueda por id

--- js/movie.js
+++ js/movie.js
@@ -29,10 +29,13 @@ const getMovieDetails = (movieid) => {
 
 const renderMovieDetails = async (movieid) => {
   const movieDetails = await getMovieDetails(movieid);
-  const { backdrop_path, poster_path,title } = movieDetails;
+  const { backdrop_path, poster_path, title, overview, genres, release_date } =
+    movieDetails;
   renderBackground(backdrop_path);
-  renderPoster(poster_path,title);
-  console.log(movieDetails);
+  renderPoster(poster_path, title);
+
+  renderMovieData(title, overview, genres, release_date);
+  renderTeaser(movieid);
 };
 
 const renderBackground = (backdrop_path) => {
@@ -43,15 +46,73 @@ const renderBackground = (backdrop_path) => {
   divMovieInfo.style.backgroundImage = `url('${urlBackground}')`;
 };
 
-const renderPoster=(poster_path,title)=>{
-   const urlPoster=`https://image.tmdb.org/t/p/original${poster_path}`;
-   const html=`<img src='${urlPoster}' class='img-fluid movie-info__poster-img' alt='${title}'>`;
-   
-   const divMovieInfo = document.querySelector(".movie-info__poster");
+const renderPoster = (poster_path, title) => {
+  const urlPoster = `https://image.tmdb.org/t/p/original${poster_path}`;
+  const html = `<img src='${urlPoster}' class='img-fluid movie-info__poster-img' alt='${title}'>`;
 
-   divMovieInfo.innerHTML = `${html}`;
-}
+  const divMovieInfo = document.querySelector(".movie-info__poster");
 
-const renderMovieInfo = (info) => {
+  divMovieInfo.innerHTML = `${html}`;
+};
+
+const renderMovieInfo = (info) => {};
+
+const renderMovieData = (title, overview, genres, release_date) => {
+  let htmlGenres = "";
+  for (g of genres) {
+    htmlGenres += `<li>${g.name}</li>`;
+  }
+  const dateSplit = release_date.split("-");
+  const year = dateSplit[0];
+
+  const html = `<h2>${title}
+  <span clas='date-any'>${year}</span>
+  <span clas='teaser' data-bs-toggle="modal" data-bs-target="#video-teaser"> <i class="bi bi-play-fill"></i> Ver trailer</span>
+  </h2>
+  <h5>General</h5>
+  <p>${overview}</p>
+  <h5>Generos</h5>
+  <ul>
+  ${htmlGenres}
   
+  </ul>
+  `;
+
+  const divMovieInfo = document.querySelector(".movie-info__data");
+
+  divMovieInfo.innerHTML = `${html}`;
+};
+
+const getTeaser = (movieid) => {
+  const url = `${URL_PATH}/3/movie/${movieid}/videos?api_key=${API_KEY}&language=es-ES`;
+  console.log(url);
+  return fetch(url)
+    .then((response) => response.json())
+    .then((result) => result.results)
+    .catch((error) => console.log(error));
+};
+
+const renderTeaser = async (movieid) => {
+  const movieTeaser = await getTeaser(movieid);
+  console.log(movieTeaser);
+  let keyVideo = "";
+  movieTeaser.forEach((element) => {
+    if (element.type === "Teaser" && element.site === "YouTube") {
+      keyVideo = element.key;
+      
+    }
+  });
+
+  let urlIframe = "";
+  if (keyVideo !== "") {
+    urlIframe = `<iframe width="100%" height="400" src="https://www.youtube.com/embed/${keyVideo}" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>`;
+  } 
+  else {
+    urlIframe=`<div>No hay nada</div>`
+  }
+
+  const divMovieInfo = document.querySelector(".video-teaser-iframe");
+
+  divMovieInfo.innerHTML = `${urlIframe}`;
+
 };
---
+++ js/now-playing.js
@@ -0,0 +1,54 @@
+const URL_PATH = "https://api.themoviedb.org";
+const API_KEY = "18d31574b22476636d52d019054b23f3";
+
+document.addEventListener("DOMContentLoaded", async () => {
+  let { page } = getUrlVars();
+  page === undefined ? (page = 1) : null;
+  renderNewMovies(page);
+});
+
+const getUrlVars = () => {
+  let vars = {};
+  let url = window.location.href.replace(
+    /[?&]+([^=&]+)=([^&]*)/gi,
+    function (m, key, value) {
+      vars[key] = value;
+    }
+  );
+  return vars;
+};
+
+const getNewMovies = (page) => {
+  const url = `${URL_PATH}/3/movie/now_playing?api_key=${API_KEY}&language=es-ES&page=${page}`;
+  return fetch(url)
+    .then((response) => response.json())
+    .then((result) => result)
+    .catch((error) => console.error(error));
+};
+
+const renderNewMovies = async (page) => {
+  const movies = await getNewMovies(page);
+  let html = "";
+
+  movies.results.forEach((movie) => {
+    const { id, title, poster_path } = movie;
+    const urlPoster = `https://image.tmdb.org/t/p/w500${poster_path}`;
+    const urlMoreInfo=`../pelicula.html?id=${id}`;
+
+    html += `<div class="card" style="width: 18rem;">
+     <a href="${urlMoreInfo}">
+   <img src="${urlPoster}" class="card-img-top" alt="${title}">
+   <div class="card-body">
+     <h5 class="card-title">${title}</h5>
+ 
+   </div>
+   </a>
+
+ </div>`;
+  });
+
+  const divListCards = document.querySelector(".list-cards");
+
+divListCards.innerHTML=`${html}`;
+
+};
